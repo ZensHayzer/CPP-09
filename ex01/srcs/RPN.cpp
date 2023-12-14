76a219af8f933ca17a7f465cf3f93c2d2ad1a798
ajeanne <ajeanne@student.42.fr>
2023-12-14 02:46:58 +0100

    LAST PUSH

--- ex01/srcs/RPN.cpp
+++ ex01/srcs/RPN.cpp
@@ -6,165 +6,65 @@
 /*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 05:53:31 by ajeanne           #+#    #+#             */
-/*   Updated: 2023/10/31 12:34:12 by ajeanne          ###   ########.fr       */
+/*   Updated: 2023/12/14 02:37:32 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-RPN::RPN() : _entry("default")  {
-    
-}
+int	calculate(std::string arg){
+	std::istringstream	list(arg);
+	std::stack<int>		resS;
+	int					check = 0;
 
-RPN::RPN(std::string const & entry) : _entry(entry) {
-    
-}
+	std::string	tmp;
+	while (list >> tmp){
+		std::stringstream	numbList(tmp);
+		int					number = 0;
+		int					_a;
+		int					_b;
 
-RPN::RPN(RPN const & src)   {
-    *this = src;
-}
-
-RPN::~RPN() {
-    
-}
-
-RPN &RPN::operator=(RPN const & src)    {
-    if (this != &src)   {
-        _entry = src.getEntry();
-        copyStackI(src.getNumbers(), _numbers);
-        copyStackC(src.getOperators(), _operators);
-    }
-    
-    return (*this);
-}
-
-std::string RPN::getEntry() const   {
-    return (_entry);
-}
-
-std::stack<int>    RPN::getNumbers() const {
-    return (_numbers);
-}
-
-std::stack<char>   RPN::getOperators() const   {
-    return (_operators);
-}
-
-void    RPN::entryOk(std::string const & entry)  {
-    std::string listChar = "0123456789+-/* ";
-    
-    for (size_t i = 0; i < entry.size(); i++)   {
-        if (i < entry.size() - 1 && isdigit(entry[i] && entry[i + 1]))
-            throw NotUnderTen();
-        if (listChar.find(entry[i]) == std::string::npos)
-            throw CharNotSupported();
-    }
-}
-
-void    RPN::fillVec(std::string const & entry)  {
-    std::string operators = "+-/*";
-    
-    for (size_t i = entry.size(); i > 0; --i)   {
-        if (isdigit(entry[i]))
-            _numbers.push(entry[i] - 48);
-        else if (operators.find(entry[i]) != std::string::npos)
-            _operators.push(entry[i]);
-    }
-    if (isdigit(entry[0]))
-        _numbers.push(entry[0] - 48);
-    else if (operators.find(entry[0]) != std::string::npos)
-        _operators.push(entry[0]);
-    
-    if (_numbers.size() != _operators.size() + 1)
-        throw WrongNumbersOperator();
-}
-
-int RPN::calculate(int a, int b, char o) {
-    switch (o)
-    {
-    case 's':
-    {
-        std::cout << "The result is : " << a << std::endl;
-        return (a);
-    }
-    case '+':
-        return (a + b);
-    case '-':
-        return (a - b);
-    case '/':
-        if (b == 0)
-            throw DivisionZero();
-        return (a / b);
-    case '*':
-        return (a * b);
-    }
-    
-    return (0);
-}
-
-void    RPN::doCalc(std::string const & entry)   {
-    long    res;
-    int     nb1;
-    int     nb2;
-    
-
-    entryOk(entry);
-    fillVec(entry);
-
-    nb1 = _numbers.top();
-    _numbers.pop();
-    if (_numbers.size() > 0)    {
-        nb2 = _numbers.top();
-        _numbers.pop();      
-    }
-    else
-    {
-        res = calculate(nb1, 0, 's');
-        return ;
-    }
-    res = calculate(nb1, nb2, _operators.top());
-    _operators.pop();
-    while(_numbers.size() >= 2) {
-        nb2 = _numbers.top();
-        _numbers.pop();
-        res = calculate(res, nb2, _operators.top());
-        _operators.pop();
-    }
-    
-    if (_numbers.size() == 1)   {
-        nb2 = _numbers.top();
-        _numbers.pop();
-        res = calculate(res, nb2, _operators.top());
-        _operators.pop();
-    }
-
-    std::cout << "The result is : " << res << std::endl;
-}
-
-void    RPN::copyStackI(std::stack<int> stack, std::stack<int> stack1)   {
-    std::stack<int> tmp;
-    size_t          size = stack.size();
-    
-    for (size_t i = 0; i < size; i++)   {
-        tmp.push(stack.top());
-        stack.pop();
-    }
-    for (size_t i = 0; i < size; i++)   {
-        stack1.push(tmp.top());
-        tmp.pop();
-    }
-}
-
-void    RPN::copyStackC(std::stack<char> stack, std::stack<char> stack1)   {
-    std::stack<char> tmp;
-    size_t          size = stack.size();
-    
-    for (size_t i = 0; i < size; i++)   {
-        tmp.push(stack.top());
-        stack.pop();
-    }
-    for (size_t i = 0; i < size; i++)   {
-        stack1.push(tmp.top());
-        tmp.pop();
-    }
+		if (numbList >> number){
+			resS.push(number);
+			check++;
+			if (check == 3){
+				std::cout<<"Error: Too much consecutive digit in arg"<<std::endl;
+				return (1);
+			}
+		}
+		else if ((tmp.find("+") != std::string::npos || tmp.find("/") != std::string::npos
+			|| tmp.find("*") != std::string::npos || tmp.find("-") != std::string::npos)
+			&& resS.size() > 1){
+			_b = resS.top();
+			resS.pop();
+			_a = resS.top();
+			resS.pop();
+			if (tmp == "+"){
+				check = 0;
+				resS.push(_a + _b);
+			}
+			else if (tmp == "-"){
+				check = 0;
+				resS.push(_a - _b);
+			}
+			else if (tmp == "*"){
+				check = 0;
+				resS.push(_a * _b);
+			}
+			else if (tmp == "/"){
+				check = 0;
+				if (_b == 0)
+					return (std::cout<<"Error: No divide by 0"<<std::endl, 1);
+				resS.push(_a / _b);
+			}
+			else
+				return (std::cout<<"Error: Wrong Operator"<<std::endl, 1);
+		}
+		else{
+			std::cout << "Error: Use number, +, -, *, /, only." << std::endl;
+			return (1);
+		}
+	}
+	std::cout << resS.top() << std::endl;
+	return (0);
 }
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,25 +6,20 @@
 /*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 05:54:29 by ajeanne           #+#    #+#             */
-/*   Updated: 2023/09/25 07:29:44 by ajeanne          ###   ########.fr       */
+/*   Updated: 2023/12/14 02:21:35 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 int main(int argc, char **argv) {
-    if (argc == 2)  {
-        RPN rpn;
-        
-        try
-        {
-            rpn.doCalc(argv[1]);
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-    }
+	if (argc == 2)
+	{
+		std::string	arg = argv[1];
+
+		if (calculate(arg))
+			return 1;
+	}
     else
         std::cout << "Error: Need 1 parameter." << std::endl;
     
